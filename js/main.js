29c6e3afb5b58d08c7779b407dd4a6672f54ff86
dyegocoqueiro <dyegocoqueiro039@gmail.com>
2025-12-28 17:30:18 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,3 +23,34 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
+const images = [
+  "../images/goku/goku-ssj2-shenlong-img1.jpeg",
+  "../images/goku/goku-ssj2-shenlong-img2.jpeg",
+  "../images/goku/goku-ssj2-shenlong-img3.jpeg"
+];
+
+let currentIndex = 0;
+
+const carouselImage = document.getElementById("carousel-image");
+const prevBtn = document.querySelector(".carousel-btn.prev");
+const nextBtn = document.querySelector(".carousel-btn.next");
+
+if (carouselImage && prevBtn && nextBtn) {
+  prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    carouselImage.style.opacity = 0;
+    setTimeout(() => {
+      carouselImage.src = images[currentIndex];
+      carouselImage.style.opacity = 1;
+    }, 200);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    carouselImage.style.opacity = 0;
+    setTimeout(() => {
+      carouselImage.src = images[currentIndex];
+      carouselImage.style.opacity = 1;
+    }, 200);
+  });
+}
